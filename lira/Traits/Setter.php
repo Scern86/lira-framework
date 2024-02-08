f6e1b7989b03887ca3d55c0ec88848db303c77d0
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-02-08 10:18:52 +0200

    Add Traits: Getter/Setter

---
+++ lira/Traits/Setter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Scern\Lira\Traits;
+
+trait Setter
+{
+    protected readonly bool $appendOnly;
+
+    public function __set(string $key, $value): void
+    {
+        if (!$this->appendOnly || !array_key_exists($key, $this->values)) $this->values[$key] = $value;
+    }
+
+    public function __unset(string $key): void
+    {
+        if (array_key_exists($key, $this->values) && !$this->appendOnly) unset($this->values[$key]);
+    }
+}
