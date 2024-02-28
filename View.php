8a637cea7e4db4f2e543d78b24736a68672537a4
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-02-28 12:49:59 +0200

    Update View.php

--- View.php
+++ View.php
@@ -8,24 +8,21 @@ class View
 {
     use Getter, Setter;
 
-    public function __construct(protected ?string $template = null, protected array $values = [], bool $appendOnly = false)
+    public function __construct(
+        public string $template = '',
+        protected array $values = [],
+        bool $appendOnly = false
+    )
     {
         $this->appendOnly = $appendOnly;
     }
 
-    public function setTemplate(string $template): void
-    {
-        if (!file_exists($template)) throw new \Exception('File not exists');
-        $this->template = $template;
-    }
-
     public function render(): string
     {
-        $result = '';
-        if (is_null($this->template)) throw new \Exception('Template is undefined');
+        if (empty($this->template)) throw new \Exception('Template is undefined');
+        if (!file_exists($this->template)) throw new \Exception('File not exists');
         ob_start();
         include $this->template;
-        $result = ob_get_clean();
-        return $result;
+        return ob_get_clean();
     }
 }
